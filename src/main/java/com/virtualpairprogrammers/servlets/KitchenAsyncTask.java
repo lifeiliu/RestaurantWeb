101d1b66672edb142a2df47460afecf747ab667a
lifei <lliu.lifei@gmail.com>
2019-06-20 11:03:30 -0400

    using asynchronous servlets

---
+++ src/main/java/com/virtualpairprogrammers/servlets/KitchenAsyncTask.java
@@ -0,0 +1,52 @@
+package com.virtualpairprogrammers.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.virtualpairprogrammers.data.MenuDao;
+import com.virtualpairprogrammers.data.MenuDaoFactory;
+import com.virtualpairprogrammers.domain.Order;
+
+public class KitchenAsyncTask implements Runnable {
+
+	private AsyncContext asyncContext;
+
+	public void setAsyncContext(AsyncContext asyncContext) {
+		this.asyncContext = asyncContext;
+	}
+
+	@Override
+	public void run() {
+		HttpServletRequest request = (HttpServletRequest) asyncContext.getRequest();
+		HttpServletResponse response = (HttpServletResponse) asyncContext.getResponse();
+
+		PrintWriter out = null;
+
+		try {
+			out = response.getWriter();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		response.setContentType("text/html");
+		Long size = Long.parseLong(request.getParameter("size"));
+
+		MenuDao dao = MenuDaoFactory.getMenuDao();
+		while (dao.getAllOrders().size() < size) {
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		Order order = dao.getOrder(size);
+		out.println("<p><strong>Next Order: </strong>" + order.toString() + "</p>");
+		out.close();
+	}
+
+}
